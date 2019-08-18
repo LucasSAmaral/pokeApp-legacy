9c7a110990ec349af38f571b030100b32b0e95a5
Lucas <lucassantosamaral@hotmail.com>
2019-08-17 22:48:45 -0300

    Adicionando estilo da pokebola

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import "./style/App.scss";
 
 function App() {
-  return (
-    <div className="App" id="burst-12">
-      <div>Teste</div>
-    </div>
-  );
+  return <div className="App" />;
 }
 
 export default App;
