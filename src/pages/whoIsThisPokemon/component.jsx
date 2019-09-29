395f1cd7ec05c5b8a52c926c2d772b7eac6b2b5b
Lucas <lucassantosamaral@hotmail.com>
2019-09-29 04:11:25 -0300

    realizando dispatch de nome e url

--- src/pages/whoIsThisPokemon/component.jsx
+++ src/pages/whoIsThisPokemon/component.jsx
@@ -2,29 +2,32 @@ import React from "react";
 import useComponentDidMount from "../../helpers/useComponentDidMount";
 import randomizeNumber from "../../helpers/randomizeNumber";
 import { store } from "../..";
+import { pathOr } from "ramda";
 import getPokemons from "../../helpers/getPokemons";
 
 export default props => {
+  const state = store.getState();
+  const pokemonName = pathOr("", ["pokemon", "pokemonName"], state);
   useComponentDidMount(() => {
     document.title = "Who Is This Pokémon?";
     const randomNumber = randomizeNumber(1, 151);
     props.onPokemonNumberFetched(randomNumber);
-    const state = store.getState();
-    Promise.resolve(getPokemons(0, 151))
+    const pokemons = getPokemons(0, 151);
+    pokemons
       .then(pokemon => {
         props.onPokemonNameFetched(pokemon[randomNumber].name);
         props.onPokemonsUrlFetched(pokemon[randomNumber].url);
-        const state = store.getState();
         console.log("Inside Promise", state);
       })
       .catch(error => {
-        throw error;
+        console.log(error);
       });
     console.log("Outside Promise", state);
   });
   return (
     <div className="container__pokeApp">
       <h1>Who Is This Pokémon?</h1>
+      <h2>{pokemonName}</h2>
     </div>
   );
 };
