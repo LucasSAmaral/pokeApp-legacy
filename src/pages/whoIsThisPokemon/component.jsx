ba88fcc9bb79cba01827bdbd73a184d23ca0208f
Lucas <lucassantosamaral@hotmail.com>
2020-01-25 00:02:41 -0300

    ajustes no componente whoIsThisPokemon

--- src/pages/whoIsThisPokemon/component.jsx
+++ src/pages/whoIsThisPokemon/component.jsx
@@ -1,23 +1,17 @@
 import React from "react";
 import useComponentDidMount from "../../helpers/useComponentDidMount";
-import { store } from "../..";
 import AppText from "../../assets/AppText/AppText.json";
 import FeatureMainTitle from "../../components/featureTitle/featureMainTitle";
 import PokeAppGame from "../../components/pokeAppGame/pokeAppGame";
 import PokemonImage from "../../components/pokemonImage/pokemonImage";
 import randomizePokemon from "../../helpers/randomizePokemon";
-import {
-  getWhoIsThisPokemonPageText,
-  getCover,
-  getPokemonImage
-} from "./whoIsThisPokemon.selector";
+import { getWhoIsThisPokemonPageText } from "./whoIsThisPokemon.selector";
 import Loading from "../../components/Loading/Loading";
 
 export default props => {
-  const state = store.getState();
   const WhoIsThisPokemonPageText = getWhoIsThisPokemonPageText(AppText);
-  const isCover = getCover(state);
-  const pokemonImageSrc = getPokemonImage(state);
+  const { whoIsThisPokemon, pokemon } = props;
+
   useComponentDidMount(() => {
     document.title = "Who Is This Pokémon?";
     randomizePokemon(props);
@@ -29,7 +23,10 @@ export default props => {
           {WhoIsThisPokemonPageText.PageTitle}
         </FeatureMainTitle>
         <Loading />
-        <PokemonImage class={isCover ? "cover" : ""} src={pokemonImageSrc} />
+        <PokemonImage
+          class={whoIsThisPokemon.cover ? "cover" : ""}
+          src={pokemon.pokemonImage}
+        />
         <div className="container__pokeButton">
           <button
             className="pokeButton"
